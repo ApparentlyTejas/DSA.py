0fe312077ee6dc1c8896505e6228146b075e133b
ApparentlyTejas <tejas.malhotra.14@gmail.com>
2025-05-22 17:39:50 +0530

    contains duplicates

--- leetcode383.py
+++ leetcode383.py
@@ -7,3 +7,20 @@ class solution:
             else: 
                 return False
         return True
+#time: O(m*n)
+#space: O(1)
+
+#optimal Solution:
+
+from collections import Counter
+class solution:
+    def Constructor(self, ransomNote: str, magazine: str) -> bool:
+        hashmap = Counter(magazine)
+        for char in ransomNote:
+            if hashmap[char] > 0:
+                hashmap[char] -= 1
+            else:
+                return False
+        return True
+#time: O(m+n)
+#space: O(1)
