9e5e53fb4a0c24e6e50ce00d5cbed0a083ec46a5
ApparentlyTejas <tejas.malhotra.14@gmail.com>
2025-05-22 17:46:20 +0530

    anagram

---
+++ leetcode217.py
@@ -0,0 +1,9 @@
+class solution:
+    def containsDuplicates(self, nums: list[int]) -> bool:
+        hashset = set()
+        for num in nums:
+            if num in hashset:
+                return True
+            else:
+                hashset.add(num)
+        return False
