7471f341e6ceeb0903a10fe750140886257f0259
ApparentlyTejas <tejas.malhotra.14@gmail.com>
2025-05-30 08:36:54 +0530

    valid parentheses

--- leetcode42.py
+++ leetcode42.py
@@ -5,15 +5,16 @@ class Solution:
         max_left = [0] * n
         max_right = [0] * n
         for i in range(n):
-            j = -i - 1
+            j = - i - 1
             max_left[i] = l_wall
             max_right[j] = r_wall
-            l_wall = max(l_wall, height[i])
-            r_wall = max(r_wall, height[j])
+            l_wall = max(l_wall , height[i])
+            r_wall = max(r_wall , height[j])
         summ = 0
         for i in range(n):
-            pot = min(l_wall[i], r_wall[i])
-            summ += max(0, pot - height[i])
+            pot = min(max_left[i], max_right[i])
+            summ += max(0 , pot - height[i])
         return summ
+
     
-    #time: O(n)  space: O(n
+    #time: O(n)  space: O(n)
